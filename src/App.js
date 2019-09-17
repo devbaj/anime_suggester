4e16ed1d6d75fb5eb1cb5285730a313079f20338
Brett Jory <dev.bjory@gmail.com>
2019-09-16 20:29:52 -0700

    api pipeline set up, renders into component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import API from './apiHandler';
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <GenreSelector></GenreSelector>
-      <TagSelector></TagSelector>
-      <MediumSelector></MediumSelector>
-      <TitleSelector></TitleSelector>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      api: new API(),
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header></Header>
+        <GenreSelector
+          api={this.state.api}
+        ></GenreSelector>
+        <TagSelector
+          api={this.state.api}
+        ></TagSelector>
+        <MediumSelector></MediumSelector>
+        <TitleSelector></TitleSelector>
+      </div>
+  )};
 }
 
 function Header() {
@@ -23,11 +36,45 @@ function Header() {
 }
 
 class GenreSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      title: null,
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.props.api.testKey();
+    this.getGenreList();
+  }
+
+  async getGenreList() {
+    console.log('entered getGenreList');
+    this.state.info = await this.props.api.testRequest(820)
+      .then(res => {
+        this.setState({loading: false, data: res});
+        this.setState({title: res.data.Media.title.english});
+      });
+    console.log(this.state.title);
+  }
+
+  renderList(data) {
+    return (
+      <ul>
+        <li>{data}</li>
+      </ul>
+    );
+  }
 
   render() {
+    const { loading, data } = this.state;
+
     return (
     <div>
         <p>GenreSelector rendered</p>
+        {loading ? "data loading" : this.renderList(this.state.title)}
         <FilterableList></FilterableList>
       </div>
     )
--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,9 +1,66 @@
-const apiInfo = require('./apiCredentials.json');
+import { ID as _ID, secret } from './apiCredentials';
 
 class ApiHandler {
-  ID = apiInfo.ID;
-  key = apiInfo.secret;
-  constructor() {
+  ID = _ID;
+  key = secret;
+  URL = 'https://graphql.anilist.co';
 
+  testKey() {
+    console.log("ID:", _ID);
+    console.log("secret:", secret);
   }
+
+  testRequest(id = 820) {
+    var query = `
+    query ($id: Int) {
+      Media (id: $id, type: ANIME) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+      }
+    }
+    `;
+    var variables = {
+      id: id
+    }
+    var url = this.URL,
+      options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        })
+      };
+    return new Promise( resolve => {
+      fetch(url, options).then( res => {
+        var data = this.handleResponse(res);
+        resolve(data);
+      })
+        .catch(this.handleError);
+    });
+  }
+
+  handleResponse(res) {
+    return res.json().then(json => {
+      return res.ok ? json : Promise.reject(json);
+    });
+  }
+
+  handleData(data) {
+    console.log(data);
+  }
+
+  handleError(err) {
+    console.error(err);
+  }
+
 }
+
+export default ApiHandler;
